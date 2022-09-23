3512a4882d02dc924bb3598ab2e103f9755dc314
Ferran Salguero Mateu <fsalgueromateu@gmail.com>
2022-09-23 11:16:44 +0200

    v1.0

---
+++ Unblocker/Global.asax.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace Unblocker
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            //AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(CustomHttpProxy.Register);
+            //FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            //RouteConfig.RegisterRoutes(RouteTable.Routes);
+            //BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+
+    public static class CustomHttpProxy
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+                name: "Proxy",
+                routeTemplate: "{*path}",
+                handler: HttpClientFactory.CreatePipeline(
+                    innerHandler: new HttpClientHandler(),
+                    handlers: new DelegatingHandler[]
+                    {
+                    new ProxyHandler()
+                    }
+                ),
+                defaults: new { path = RouteParameter.Optional },
+                constraints: null
+            );
+        }
+    }
+
+    public class ProxyHandler : DelegatingHandler
+    {
+        private static HttpClient client = new HttpClient();
+
+        protected override async Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            var query = request.RequestUri.ParseQueryString();
+            var requestedUrl = query["q"];
+
+            if (string.IsNullOrEmpty(requestedUrl)) return new HttpResponseMessage();
+
+            var forwardUri = new UriBuilder(requestedUrl);
+            //forwardUri.Host = "localhost";
+            //forwardUri.Port = 62904;
+            request.RequestUri = forwardUri.Uri;
+
+            if (request.Method == HttpMethod.Get)
+            {
+                request.Content = null;
+            }
+
+            request.Headers.Add("X-Forwarded-Host", request.Headers.Host);
+            request.Headers.Host = forwardUri.Host;
+            var response = await client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+            return response;
+        }
+    }
+}
